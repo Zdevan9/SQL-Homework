73a93063ef9c6a2dc2c3c44edba0f2dfcd04cfad
Stefan Roguzharov <124082123+Zdevan9@users.noreply.github.com>
2023-05-19 18:03:44 +0200

    Update Homework1.sql

--- Homework 1/Homework1.sql
+++ Homework 1/Homework1.sql
@@ -1,31 +1,5 @@
-create table Student (
-id int identity (1,1) not null,
-FirstName nvarchar(20) not null,
-LastName nvarchar(20) not null,
-DateofBirth date not null,
-EnrolledDate date null,
-Gender nchar(1) null,
-NationalIdNumber nvarchar(10) not null,
-StudentCardNumber nvarchar(10) not null,
-);
-
-create table Teacher (
-Id int identity(1,1) not null,
-FirstName nvarchar(20) not null,
-LastName nvarchar(20) not null,
-DateofBirth date not null,
-AcademicRank nvarchar(10) not null,
 HireDate date null,
-);
-
-create table Grade (
-id int identity(1,1) not null,
-StudentID int identity(1,1) not null,   --Not sure if these need to be taken with foreign keys from the other tables
-CourseID int identity(1,1) not null,
-TeacherID int identity(1,1) not null,
-Grade int not null,
-Comment nvarchar(max) null,
-CreatedDate date null,
+primary key (id),
 );
 
 create table Course (
@@ -34,20 +8,39 @@ Id int identity(1,1) not null,
 Credit int not null,
 AcademicYear int not null,
 Semester int not null,
+primary key (id),
 );
 
-create table GradeDetails (
-Id int identity(1,1) not null,
-GradeId int not null,                 --Also these
-AchievementTypeID int not null,
-AchievementPoints int not null,
-AchievementMaxPoints bigint not null,
-AchievementDate date not null,
+create table Grade (
+id int identity(1,1) not null,
+StudentID int identity(1,1) not null,  
+CourseID int identity(1,1) not null,
+TeacherID int identity(1,1) not null,
+Grade int not null,
+Comment nvarchar(max) null,
+CreatedDate date null,
+primary key (id),
+	FOREIGN KEY (StudentID) REFERENCES Student(id),
+	FOREIGN KEY (CourseID) REFERENCES Course(Id),
+	FOREIGN KEY (TeacherID) REFERENCES Teacher(Id),
+
 );
 
-create table AchivementType (
+create table AchievementType (
 Id int identity(1,1) not null,
 [Name] nvarchar(20) not null,
 [Description] nvarchar(max) null,
 ParticipationRate bigint null,
+primary key (id)
+);
+
+create table GradeDetails (
+Id int identity(1,1) not null,
+GradeId int not null,                 
+AchievementTypeID int not null,
+AchievementPoints int not null,
+AchievementMaxPoints bigint not null,
+AchievementDate date not null,
+    FOREIGN KEY (GradeId) REFERENCES Grade(id),
+    FOREIGN KEY (AchievementTypeID) REFERENCES AchievementType(Id)
 );
